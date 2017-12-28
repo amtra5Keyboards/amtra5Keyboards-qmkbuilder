aa348b71e8593567b2e48d851c42391e032f495b
Angus Trau <angus.amtra5@gmail.com>
2017-12-29 04:11:24 +1100

    Add sydney meetup 2017 #2 preset

--- src/const/presets.js
+++ src/const/presets.js
@@ -3,5 +3,6 @@ module.exports = {
 	'amtrapad10rev1': 'amtraPad-10 Rev.1',
 	'amtrapad16rev1': 'amtraPad-16 Rev.1',
 	'melbourne2017-3': 'Melbourne Meetup 2017 #3 macropad',
-	'sydney2017': 'Sydney Meetup 2017 macropad'
+	'sydney2017': 'Sydney Meetup 2017 macropad',
+	'sydney2017-2': 'Sydney Meetup 2017 #2 macropad'
 };
